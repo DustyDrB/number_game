c280446cceedc9733afa648335c595a168e0daf6
meadch <meadch@gmail.com>
2016-05-18 17:59:12 -0700

    adds ajax

--- server.py
+++ server.py
@@ -33,20 +33,6 @@ def check_answer():
         session['result']['text'] = "TOO HIGH!"
 
     return redirect('/')
-
-    ## Here I am trying to gather the form input from index.html
-    ## and compare it to a random number.
-    # if not 'num' in session:
-    #     session['num'] = random.randrange(0, 101)
-    # answer = request.form['guess']
-    # if session['num'] > int(answer):
-    #     if session['num'] > answer:
-    #         statement = "Too low! Try again!"
-    #     elif session['num'] < answer:
-    #         statement = "Too high! Try again!"
-    #     elif session['num'] == answer:
-    #         statement = "Correct!"
-    #
-    # return redirect('/', data = session['statement'])
+    
 if __name__ =="__main__":
     app.run(debug=True)
